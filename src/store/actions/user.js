9d63813444574f2edf9b4cf8e7b5a9b6eb5c3cba
mian kashif <miankashifmehmood@gmail.com>
2024-10-26 21:37:51 +0500

    before deploy

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,6 +1,6 @@
-import { userActions } from "../reducers/userReducer"
+import { userActions } from "../reducers/userReducer";
 
-export const logout =()=>(dispatch)=>{
-    dispatch(userActions.resetUserInfo())
-    localStorage.removeItem('account')
-}
+export const logout = () => (dispatch) => {
+  dispatch(userActions.resetUserInfo());
+  localStorage.removeItem("account");
+};
